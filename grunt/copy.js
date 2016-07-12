187b33004b35bfd7fcb783cfda5d4265a462e656
Martin Michálek <michalek@shortcat.cz>
2016-07-12 14:47:55 +0200

    Grunt js - copy a uglify

---
+++ grunt/copy.js
@@ -0,0 +1,11 @@
+// copy: kopirovani souboru
+// ----------------------
+
+module.exports = {
+  bootstrapJS: {
+    files: {
+      'src/js/bootstrap.js': 
+      ['node_modules/bootstrap-sass/assets/javascripts/bootstrap.js']	
+    }
+  }
+};
---
+++ grunt/uglify.js
@@ -0,0 +1,12 @@
+// uglify: zmenseni JS
+// ----------------------
+
+module.exports = {
+  default: {
+    files: {
+      'dist/js/script.js': [
+      	'src/js/bootstrap.js'
+      ]	
+    }
+  }
+};
